3250b61199c03a31d84f5a7163194ad9ab589504
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-09-21 17:52:44 +0300

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleFocus(){
+        console.log("Good")
+    }
+
+    function handleBlur(){
+        console.log("Hey! All Eyes On Me")
+    }
+    return(
+        <div>
+            <button onClick={handleFocus} onBlur={handleBlur}>EyesOnMe</button>
+        </div>
+    )
+
+
+}
+export default EyesOnMe;
